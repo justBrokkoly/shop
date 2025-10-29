dca15c867bda4b7cc74518ca08721b254f572636
Kirill <kirinator3@gmail.com>
2025-10-29 22:56:18 +0300

    [KIRILL-KUSTOV] 3 exercise

---
+++ inventory-api/src/main/java/ru/innotech/inventoryapi/config/TransactionOutboxProperties.java
@@ -0,0 +1,17 @@
+package ru.innotech.inventoryapi.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+@ConfigurationProperties("outbox")
+@Data
+class TransactionOutboxProperties {
+  private Duration repeatEvery;
+  private boolean useJackson;
+  private Duration attemptFrequency;
+  private int blockAfterAttempts;
+}
--- payment-api/src/main/java/ru/innotech/paymentapi/adapters/kafka/PaymentCommandListener.java
+++ payment-api/src/main/java/ru/innotech/paymentapi/adapters/kafka/PaymentCommandListener.java
@@ -3,9 +3,7 @@ package ru.innotech.paymentapi.adapters.kafka;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.math.BigDecimal;
-import java.time.Instant;
-import java.util.UUID;
+import com.gruelbox.transactionoutbox.TransactionOutbox;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +16,11 @@ import ru.innotech.paymentapi.core.model.Payment;
 import ru.innotech.paymentapi.core.model.shared.MessageEnvelope;
 import ru.innotech.paymentapi.core.model.shared.PaymentAuthorizedEventPayload;
 import ru.innotech.paymentapi.core.model.shared.PaymentCancelledEventPayload;
+import ru.innotech.paymentapi.core.model.shared.PaymentFailedEventPayload;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
 
 @Slf4j
 @Component
@@ -27,61 +30,101 @@ public class PaymentCommandListener {
     private final PaymentRepository paymentRepository;
     private final ObjectMapper mapper;
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final TransactionOutbox transactionOutbox;
 
     @Value("${topics.payment.replies}")
-    private String paymentReplies;
+    private String paymentTopic;
 
-    @KafkaListener(topics = "${topics.payment.commands}")
+    @KafkaListener(topics = "${topics.order.topic}")
     @Transactional
     public void onCommands(String raw) throws JsonProcessingException {
         log.info("Message {} was received", raw);
         JsonNode node = mapper.readTree(raw);
         String type = node.get("type").asText();
+        JsonNode payload = node.get("payload");
+        String orderId = payload.get("orderId").asText();
+        BigDecimal amount = payload.get("amount").decimalValue();
+        if ("OrderCreated".equals(type)) {
+            try {
+                var paymentCheck = checkPayment(amount);
+                if (paymentCheck) {
+                    handleSuccessPayment(orderId, amount);
+                } else {
+                    handleCanceledPayment(orderId);
+                }
 
-        if ("PaymentAuthorizeCommand".equals(type)) {
-            JsonNode payload = node.get("payload");
-            String orderId = payload.get("orderId").asText();
-            BigDecimal amount = payload.get("amount").decimalValue();
-
+            } catch (Exception exception) {
+                log.error("ex", exception);
+                handleFailedPayment(orderId, exception.getMessage());
+            }
+        }
+        if ("OrderCancelled".equals(type)) {
             Payment p = paymentRepository.findByOrderId(orderId).orElseGet(Payment::new);
             p.setOrderId(orderId);
-            p.setAmount(amount);
-
-            p.setStatus("AUTHORIZED");
+            p.setStatus("REFUND");
             paymentRepository.save(p);
-
-            MessageEnvelope<PaymentAuthorizedEventPayload> evt = MessageEnvelope.<PaymentAuthorizedEventPayload>builder()
-                    .messageId(UUID.randomUUID().toString())
-                    .sagaId(node.get("sagaId").asText(null))
-                    .orderId(orderId)
-                    .type("PaymentAuthorizedEvent")
-                    .timestamp(Instant.now())
-                    .payload(new PaymentAuthorizedEventPayload(orderId))
-                    .build();
-
-            kafkaTemplate.send(paymentReplies, orderId, mapper.writeValueAsString(evt));
-            log.info("Message {} was sent", mapper.writeValueAsString(evt));
         }
+    }
 
-        if ("PaymentCancelCommand".equals(type)) {
-            JsonNode payload = node.get("payload");
-            String orderId = payload.get("orderId").asText();
-            Payment p = paymentRepository.findByOrderId(orderId).orElseGet(Payment::new);
-            p.setOrderId(orderId);
-            p.setStatus("CANCELLED");
-            paymentRepository.save(p);
+    private void handleSuccessPayment(String orderId, BigDecimal amount) throws JsonProcessingException {
+        Payment p = paymentRepository.findByOrderId(orderId).orElseGet(Payment::new);
+        p.setOrderId(orderId);
+        p.setAmount(amount);
+        p.setStatus("AUTHORIZED");
+        paymentRepository.save(p);
 
-            MessageEnvelope<PaymentCancelledEventPayload> evt = MessageEnvelope.<PaymentCancelledEventPayload>builder()
-                    .messageId(UUID.randomUUID().toString())
-                    .sagaId(node.get("sagaId").asText(null))
-                    .orderId(orderId)
-                    .type("PaymentCancelledEvent")
-                    .timestamp(Instant.now())
-                    .payload(new PaymentCancelledEventPayload(orderId))
-                    .build();
+        MessageEnvelope<PaymentAuthorizedEventPayload> evt = MessageEnvelope.<PaymentAuthorizedEventPayload>builder()
+                .messageId(UUID.randomUUID().toString())
+                .orderId(orderId)
+                .type("PaymentAuthorizedEvent")
+                .timestamp(Instant.now())
+                .payload(new PaymentAuthorizedEventPayload(orderId))
+                .build();
 
-            kafkaTemplate.send(paymentReplies, orderId, mapper.writeValueAsString(evt));
-            log.info("Message {} was sent", mapper.writeValueAsString(evt));
-        }
+        transactionOutbox.schedule(getClass()).sendEvent(orderId, evt);
+    }
+
+    private void handleCanceledPayment(String orderId) throws JsonProcessingException {
+        Payment p = paymentRepository.findByOrderId(orderId).orElseGet(Payment::new);
+        p.setOrderId(orderId);
+        p.setStatus("CANCELLED");
+        paymentRepository.save(p);
+
+        MessageEnvelope<PaymentCancelledEventPayload> evt = MessageEnvelope.<PaymentCancelledEventPayload>builder()
+                .messageId(UUID.randomUUID().toString())
+                .orderId(orderId)
+                .type("PaymentCancelledEvent")
+                .timestamp(Instant.now())
+                .payload(new PaymentCancelledEventPayload(orderId))
+                .build();
+
+        transactionOutbox.schedule(getClass()).sendEvent(orderId, evt);
+    }
+
+    private void handleFailedPayment(String orderId, String message) throws JsonProcessingException {
+        Payment p = paymentRepository.findByOrderId(orderId).orElseGet(Payment::new);
+        p.setOrderId(orderId);
+        p.setStatus("FAILED");
+        paymentRepository.save(p);
+
+        MessageEnvelope<PaymentFailedEventPayload> evt = MessageEnvelope.<PaymentFailedEventPayload>builder()
+                .messageId(UUID.randomUUID().toString())
+                .orderId(orderId)
+                .type("PaymentFailedEvent")
+                .timestamp(Instant.now())
+                .payload(new PaymentFailedEventPayload(orderId, message))
+                .build();
+
+        transactionOutbox.schedule(getClass()).sendEvent(orderId, evt);
+    }
+
+    <T> void sendEvent(String orderId, MessageEnvelope<T> event) throws JsonProcessingException {
+        kafkaTemplate.send(paymentTopic, orderId, mapper.writeValueAsString(event));
+        log.info("Message {} was sent", mapper.writeValueAsString(event));
+    }
+
+    private boolean checkPayment(BigDecimal amount) {
+        //проверка на баланс и прочее
+        return true;
     }
 }
